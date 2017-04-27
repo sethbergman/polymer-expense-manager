80ad664e96a9da55d00621b0b197bcb8eaf60f64
Seth Bergman <seth.atxwebs@gmail.com>
2017-04-27 07:56:34 -0500

    deploy

---
+++ sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  staticFileGlobs: [
+    'index.html',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'images/default-receipt.png'
+  ],
+  navigateFallback: 'index.html',
+  stripPrefix: '',
+  replacePrefix: '.'
+};
